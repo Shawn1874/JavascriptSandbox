286f767d2f8af53d61b546ff85ac8ee08759720d
sdfox <sdfox1874@gmail.com>
2021-02-14 15:42:38 -0800

    Commit changes to support new Array Tests

--- MochaChaiExamples/test/ArraysTests.js
+++ MochaChaiExamples/test/ArraysTests.js
@@ -8,21 +8,41 @@ var chai = require('chai'),
 chai.should();
 
 describe('Tests for basic array initialization and element access', function () {
-   it('should return Nan when the input cannot be parsed', function () {
+   let fibonacci = [2, 3, 5, 8,  13];
+   it('should return the value at specified index', function () {
+      fibonacci[2].should.eql(5);
+
    });
 
-   it('should a valid integers if the input can be converted and the radix is within the range of 2 and 36', function () {
-
+   it('should add elements using [], push, or shift', function () {
+      fibonacci[fibonacci.length] = 21;
+      fibonacci.push(34);
+      fibonacci.unshift(1);
+      fibonacci.length.should.eql(8);
+      fibonacci[0].should.eql(1);
    });
 });
 
-describe('pending', function () {
-   it('should return Nan when the input cannot be parsed', function () {
+describe('Tests for joining and searching arrays', function () {
+   let words = ["we", "hold", "these", "truths", "to be", "self-evident"];
+   let teams = ["49ers", "chargers", "dolphins", "rams", "chiefs"]
+
+   it('Should join the words of the array into a sentence', function () {
+      let sentence = words.join(" ");
+      sentence.should.eql("we hold these truths to be self-evident");
    });
 
-   it('should return a valid number if the input can be converted', function () {
+   it('should return a the team name if found', function () {
+      let team = teams.indexOf("49ers");
+      team.should.eql(0);
    });
 
-   it('should return a valid number for characters up to the first one that can\'t be converted', function () {
+   function cTeams(name) {
+      return name[0] == 'c';
+   }
+
+   it('should return a all teams that have name starting with a c', function () {
+      let result = teams.filter(cTeams);
+      result.length.should.eql(2);
    });
 });
